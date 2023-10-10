478ef62c5cc5d63e4be0301659fab086b7c46f16
AnaByo <114250758+AnaByo@users.noreply.github.com>
2023-10-09 21:05:49 -0300

    Update Bovina index.js

--- src/screens/Receitas/Bovina/index.js
+++ src/screens/Receitas/Bovina/index.js
@@ -1,57 +1,57 @@
 import {
-  Text,
-  View,
-  StyleSheet,
-  ScrollView,
-  TouchableHighlight,
-  Image
-} from 'react-native';
-import CardComponent from '../../../components/Cards/Opcoes/index';
+    Text,
+    View,
+    StyleSheet,
+    ScrollView,
+    TouchableHighlight,
+    Image
+  } from 'react-native';
+  import CardComponent from '../../../components/Cards/Opções/index';
+  
+  export default function ReceitasBovina() {
+    return (
+      <View style={styles.container}>
+        <ScrollView>
+            <View style={styles.top}>
+                <View style={styles.containerTitle}>
+                    <Text style={styles.title}>Receitas</Text>
+                    <Text style={[styles.title, { fontSize: 55, }]}>Bovina</Text>
+                </View>
 
-export default function Bovina({navigation}) {
-  return (
-    <View style={styles.container}>
-      <ScrollView>
-          <View style={styles.top}>
-              <View style={styles.containerTitle}>
-                  <Text style={styles.title}>Receitas</Text>
-                  <Text style={[styles.title, { fontSize: 55, }]}>Bovina</Text>
-              </View>
+                <Image source = {require('../../../../assets/img/boi.png')} style={styles.boi}/>
+            </View>
+            <TouchableHighlight>
+                <CardComponent />
+            </TouchableHighlight>
+        </ScrollView>
+      </View>
+    );
+  }
 
-              <Image source = {require('../../../../assets/img/boi.png')} style={styles.boi}/>
-          </View>
-          <TouchableHighlight>
-              <CardComponent selectedConst={receitasBovina} />
-          </TouchableHighlight>
-      </ScrollView>
-    </View>
-  );
-}
-
-
+  
 const styles = StyleSheet.create({
-  container: {
-      flex: 1,
-      backgroundColor: 'black'
-  },
-  containerTitle:{
-      flexDirection: 'column',
-      marginTop: 15,
-      paddingLeft: 20,
-  },
-  title:{
-      color: 'white',
-      fontSize: 30,
-      fontFamily: 'InriaSans_700Bold',
-  },
-  subtitle: {
-      color: 'white',
-      fontFamily: 'InriaSans_700Bold',
+    container: {
+        flex: 1,
+        backgroundColor: 'black'
+    },
+    containerTitle:{
+        flexDirection: 'column',
+        marginTop: 15,
+        paddingLeft: 20,
+    },
+    title:{
+        color: 'white',
+        fontSize: 30,
+        fontFamily: 'InriaSans_700Bold',
+    },
+    subtitle: {
+        color: 'white',
+        fontFamily: 'InriaSans_700Bold',
 
-  },
-  top: {
-      justifyContent: 'space-between',
-      flex: 1,
-      flexDirection: 'row',
-  },
-});
+    },
+    top: {
+        justifyContent: 'space-between',
+        flex: 1,
+        flexDirection: 'row',
+    },
+  });
