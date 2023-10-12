2fff6c9ac33ab353bf3c2c0c7b1caaee08a741f6
Edgard Anjos <86852670+vilaopvp1@users.noreply.github.com>
2023-10-12 02:18:22 -0300

    Merge branch 'master' into edgard_test2
    Merge branch 'master' into edgard_test2

--- src/components/PreviewResults/index.js
+++ src/components/PreviewResults/index.js
@@ -1,36 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
-import colors from '../../colors';
+import colors from "../../colors";
+import { useValueContext } from '../../contexts/values';
 import { useThemeContext } from '../../contexts/theme';
 
-export default function PreviewResults({ dia, data, colorText = 'light' }) {
-  const { theme } = useThemeContext();
-  const themeColor = theme === 'light' ? colors.primary : colors.light;
-  const colorSelection = colorText === 'red' ? themeColor : colors.light;
 
-  return (
-    <View style={styles.container}>
-      <Text style={[styles.data, { color: colorSelection }]}>06/08/2023</Text>
-      <View style={styles.convidadosSection}>
-        <MaterialCommunityIcons
-          name="face-man-outline"
-          size={18}
-          color={colorSelection}
-        />
-        <Text style={[styles.convidados, { color: colorSelection }]}>1</Text>
-        <MaterialCommunityIcons
-          name="face-woman-outline"
-          size={18}
-          color={colorSelection}
-        />
-        <Text style={[styles.convidados, { color: colorSelection }]}>1</Text>
-        <MaterialIcons name="child-care" size={18} color={colorSelection} />
-        <Text style={[styles.convidados, { color: colorSelection }]}>1</Text>
-        <MaterialIcons name="people-alt" size={18} color={colorSelection} />
-        <Text style={[styles.convidados, { color: colorSelection }]}>1</Text>
-      </View>
-    </View>
-  );
+export default function PreviewResults({dia, data, colorText = 'light'}){
+    const { theme } = useThemeContext();
+    const { value } = useValueContext(); // Adicionado para acessar o contexto de valores   
+    const currentDate = new Date(); // Traz a data de hoje
+    const formattedDate = `${currentDate.getDate()}/${currentDate.getMonth() + 1}/${currentDate.getFullYear()}`; // seta a forma da data
+    const themeColor = theme === 'light' ? colors.primary : colors.light;
+    const colorSelection = colorText === 'red' ? themeColor : colors.light;
+
+    return(
+        <View style={styles.container}>
+            <Text style={styles.data}>{formattedDate}</Text>
+            <View style={styles.convidadosSection}>
+                <MaterialCommunityIcons name='face-man-outline' size={18}  color={colorSelection}/>
+                <Text style={styles.convidados}>{value.convidados.homens}</Text>
+                <MaterialCommunityIcons name='face-woman-outline' size={18}  color={colorSelection}/>
+                <Text style={styles.convidados}>{value.convidados.mulheres}</Text>
+                <MaterialIcons name='child-care' size={18}  color={colorSelection}/>
+                <Text style={styles.convidados }>{value.convidados.criancas}</Text>
+                <MaterialIcons name='people-alt' size={18} color={colorSelection}/>
+                <Text style={styles.convidados}>{value.convidados.total}</Text>
+            </View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
